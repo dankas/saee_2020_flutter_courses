811ca895f28e0d54ed58a7de5bb1f6c5c8073614
Sthefano Schiavon <sthefanoss@gmail.com>
2020-09-07 19:04:32 -0300

    add course projects

---
+++ initial_level/android/app/src/main/kotlin/com/course/initial_level/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.course.initial_level
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ intermediate_level/android/app/src/main/kotlin/com/course/intermediate_level/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.course.intermediate_level
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

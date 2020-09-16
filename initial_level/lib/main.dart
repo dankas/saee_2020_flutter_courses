fb8294413a964fc1595db1d19f0794d21ed795a7
sthefano <sthefanoss@gmail.com>
2020-09-16 02:25:00 -0300

    create bakery cart example

--- initial_level/lib/main.dart
+++ initial_level/lib/main.dart
@@ -5,113 +5,196 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Map> _mercadorias;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<int> _quantidades;
+
+  @override
+  void initState() {
+    _mercadorias = [
+      {
+        "title": "Pão cacetinho",
+        "description": "Um pão branco e delicioso.",
+        "price": 0.50,
+        "imageUrl":
+            "https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/10/22/pao-branco-pode.jpg"
+      },
+      {
+        "title": "Pão com grãos",
+        "description": "Um pão saudável.",
+        "price": 1.0,
+        "imageUrl":
+            "https://portal-amb-imgs.clubedaana.com.br/2018/11/P%C3%A3o-de-gr%C3%A3os-ana-maria-600x400.jpg"
+      },
+      {
+        "title": "Pão com gergelim",
+        "description":
+            "Ótimo para fazer hamburguer, e ficar ciscando gergelim depois.",
+        "price": 1.0,
+        "imageUrl":
+            "https://www.congebras.com.br/wp-content/uploads/2018/07/10854-1024x660.jpg"
+      },
+      {
+        "title": "Pão australiano",
+        "description": "Um pão doce com cacau.",
+        "price": 2.50,
+        "imageUrl":
+            "https://s2.glbimg.com/3cbQ98gqhTsNAfvhhZ-R2o3s3D4=/0x0:652x434/984x0/smart/filters:strip_icc()/g.glbimg.com/og/gs/gsat5/f/thumbs/materia/2019/04/08/1553872667808-IMG_9965.jpg"
+      },
+      {
+        "title": "Coca cola",
+        "description": "Uma garrafa de 2l de um líquido preto.",
+        "price": 8.50,
+        "imageUrl":
+            "https://trimais.vteximg.com.br/arquivos/ids/995447-1000-1000/foto_original.jpg?v=637284348483970000"
+      },
+    ];
+    _quantidades = List<int>.generate(_mercadorias.length, (_) => 0);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Meu carrinho"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              itemCount: _mercadorias.length,
+              itemBuilder: (context, index) => ListTile(
+                title: Text(_mercadorias[index]['title'],
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+                subtitle: Row(
+                  children: [
+                    IconButton(
+                        icon: Icon(Icons.remove),
+                        onPressed: () {
+                          if (_quantidades[index] > 0)
+                            setState(() {
+                              _quantidades[index]--;
+                            });
+                        }),
+                    Text(_quantidades[index].toString()),
+                    IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () {
+                          setState(() {
+                            _quantidades[index]++;
+                          });
+                        })
+                  ],
+                ),
+                trailing: Text(
+                  "R\$ ${_mercadorias[index]['price'].toStringAsFixed(2)}",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                ),
+                leading: Hero(
+                  tag: index,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => PaginaMercadoria(
+                            descricao: _mercadorias[index]['description'],
+                            nome: _mercadorias[index]['title'],
+                            imageUrl: _mercadorias[index]['imageUrl'],
+                            tag: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          NetworkImage(_mercadorias[index]['imageUrl']),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Container(
+            height: 50,
+            color: Colors.grey,
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Total: R\$ ${_calculoConta()}",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                RaisedButton(
+                  child: Text('Pagar'),
+                  onPressed: () {
+                    for (int i = 0; i < _quantidades.length; i++)
+                      _quantidades[i] = 0;
+                    setState(() {});
+                  },
+                )
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  String _calculoConta() {
+    double sum = 0;
+    for (int i = 0; i < _quantidades.length; i++)
+      sum += _quantidades[i] * _mercadorias[i]['price'];
+    return sum.toStringAsFixed(2);
+  }
+}
+
+class PaginaMercadoria extends StatelessWidget {
+  final String nome;
+  final String descricao;
+  final String imageUrl;
+  final int tag;
+
+  PaginaMercadoria({this.nome, this.descricao, this.imageUrl, this.tag});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sobre $nome"),
+      ),
+      body: Column(
+        children: [
+          Hero(tag: tag, child: Image.network(imageUrl)),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Descrição: "),
+          Text(descricao)
+        ],
+      ),
     );
   }
 }
